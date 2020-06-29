8e38c0db0ac6b118acf8580bf03d20f9eb42746f
LoaezaMagda <loaeza.magdalena@gmail.com>
2020-06-29 17:04:17 -0500

    agregamos proyecto

---
+++ app/models/customer.rb
@@ -0,0 +1,6 @@
+class Customer < ActiveRecord::Base 
+  has_one :address 
+  has_many :invoices
+  delegate :street, :city, :state, :zip_code, :to => :address 
+end
+    
---
+++ app/models/invoce.rb
@@ -0,0 +1,5 @@
+class Invoice < ActiveRecord::Base 
+  belongs_to :customer
+  delegate :name, :street, :city, :state, :zip_code, :to     => :customer, :prefix => true 
+end
+    
